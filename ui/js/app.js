842d148408c59008b5feec6cc630089ce010b58b
Jon Fairbanks <fairplay89@gmail.com>
2022-01-28 23:22:22 -0800

    Initial UI

---
+++ ui/js/app.js
@@ -0,0 +1,20 @@
+console.log("Fetching data...")
+
+fetch('http://192.168.4.4:5000/api/parse/cars')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error:', err);
+  });
